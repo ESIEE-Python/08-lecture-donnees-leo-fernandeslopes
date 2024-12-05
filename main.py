e1d8b81de5f0e4f3e9af3892871cf45d6f2af22e
leo-fernandeslopes <leo.fernandeslopes@edu.esiee.fr>
2024-12-05 09:22:59 +0000

    Exercice Csv Récup et Lecture de données

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-#### Imports et définition des variables globales
-import random
+""" EXO CSV RECUPERATION DE DONNEES
+Imports et définition des variables globales """
+
+### import random
 
 FILENAME = "listes.csv"
 
@@ -14,34 +16,57 @@ def read_data(filename):
     Returns:
         list: le contenu du fichier (1 list par ligne)
     """
-    l = []
-    return l
+    try:
+        with open(filename, mode="r", encoding="utf-8") as file:
+            # Convertir lignes en liste d'entiers
+            return [list(map(int, line.strip().split(';'))) for line in file.readlines()]
+
+    except FileNotFoundError:
+        print(f"Erreur : Le fichier {filename} est introuvable.")
+        return []
 
 def get_list_k(data, k):
-    l = []
-    return l
+    """ Renvoie la k-ieme liste """
+    if 0 <= k < len(data):
+        return data[k]
+    print(f"Erreur : Indice {k} out limites.")
+    return None
 
 def get_first(l):
+    """ Renvoie le premier element """
+    if l :
+        return l[0]
     return None
 
 def get_last(l):
+    """ Renvoie le dernier element """
+    if l :
+        return l[-1]
     return None
 
 def get_max(l):
+    """ Renvoie l'element maximum """
+    if l :
+        return max(l)
     return None
 
 def get_min(l):
+    """ Renvoie l'element minimum """
+    if l :
+        return min(l)
     return None
 
 def get_sum(l):
-    return None
+    """ Renvoie la somme de la liste """
+    if l :
+        return sum(l)
+    return 0
 
 
 #### Fonction principale
 
-
 def main():
-    pass
+    """ Fonction principale main() """
     # data = read_data(FILENAME)
     # for i, l in enumerate(data):
     #     print(i, l)
